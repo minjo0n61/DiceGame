5727dced8796ef199490581864b38379c3663c77
MinJoon_Choi <minjoon1998@gmail.com>
2024-02-05 21:07:34 +0900

    feat: Add Button to handle dice color and number
    Use useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import { useState } from 'react';
+import Button from './Button';
 import Dice from './Dice';
 
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
+
 function App() {
+  const [color, setColor] = useState('red');
+  const [num, setNum] = useState(1);
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+    // eslint-disable-next-line
+    console.log(nextNum);
+  };
+
+  const handleClearClick = () => {
+    setNum(1);
+    // eslint-disable-next-line
+    console.log('clear!');
+  };
+
+  const handleColor = () => {
+    if (color === 'red') {
+      setColor('blue');
+    } else {
+      setColor('red');
+    }
+    // eslint-disable-next-line
+    console.log('색 변경!');
+  };
+
   return (
     <div>
-      <Dice />
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+        <Button onClick={handleColor}>색 변경</Button>
+      </div>
+      <Dice color={color} num={num} />
     </div>
   );
 }
